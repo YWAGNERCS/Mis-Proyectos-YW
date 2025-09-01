28ad85e5e83aedfadd14526c8d2eb9073a959c72
YWAGNERCS <yoelwagnercs@gmail.com>
2025-09-01 12:36:02 -0500

    Asistencia

--- Asistencia/src/main/java/pe/edu/upeu/asistencia/Aplication.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.upeu.asistencia;
-
-public class Aplication {
-    public static void main(String[] args) {
-        System.out.println("Iniciamos apliciacion DMP");
-        AsistenciaApplication.main(args);
-    }
-}
